b9aa6de9f2c0294a320fee91257ffcb08692f3bd
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-01-21 01:37:21 +0200

    добавляет изменение темы

---
+++ src/js/theme.js
@@ -0,0 +1,16 @@
+const themeSwitchToggleRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+themeSwitchToggleRef.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  event.preventDefault();
+
+  if (!themeSwitchToggleRef.checked) {
+    bodyRef.classList.add('dark-theme');
+    return;
+  }
+
+  bodyRef.classList.remove('dark-theme');
+  bodyRef.classList.add('light-theme');
+}
