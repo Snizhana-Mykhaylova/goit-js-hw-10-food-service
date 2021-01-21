d340cc2918cb063bd83ecd103181664801946bf4
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-01-21 22:11:58 +0200

    добавляет хранение темы на localStorage

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,9 @@
 const themeSwitchToggleRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 themeSwitchToggleRef.addEventListener('change', changeTheme);
 
@@ -7,10 +11,20 @@ function changeTheme() {
   event.preventDefault();
 
   if (themeSwitchToggleRef.checked) {
-    bodyRef.classList.add('dark-theme');
+    bodyRef.classList.add(Theme.DARK);
+    window.localStorage.setItem('theme', Theme.DARK);
     return;
   }
 
-  bodyRef.classList.remove('dark-theme');
-  bodyRef.classList.add('light-theme');
+  bodyRef.classList.remove(Theme.DARK);
+  bodyRef.classList.add(Theme.LIGHT);
+  window.localStorage.setItem('theme', Theme.LIGHT);
+}
+
+const savedThema = window.localStorage.getItem('theme');
+
+if (savedThema === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
+  window.localStorage.setItem('theme', Theme.DARK);
+  themeSwitchToggleRef.checked = true;
 }
